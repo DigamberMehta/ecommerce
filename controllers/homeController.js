baa8aa42cfb22453e73e5f071adb97cd63df61f9
Digamber Mehta <digambermehta2603@gmail.com>
2024-07-25 21:37:50 +0530

    fixes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,37 +1,37 @@
-const Product = require('../models/product');
-const UserInteraction = require('../models/userInteraction');
+// const Product = require('../models/product');
+// const UserInteraction = require('../models/userInteraction');
 
-// Handle rendering the home page and product recommendations
-exports.renderHomePage = async (req, res) => {
-  try {
-    const products = await Product.find();
-    let recommendedProducts = [];
+// // Handle rendering the home page and product recommendations
+// exports.renderHomePage = async (req, res) => {
+//   try {
+//     const products = await Product.find();
+//     let recommendedProducts = [];
 
-    if (req.isAuthenticated()) {
-      const userId = req.user._id;
-      const interactions = await UserInteraction.find({ user: userId });
+//     if (req.isAuthenticated()) {
+//       const userId = req.user._id;
+//       const interactions = await UserInteraction.find({ user: userId });
 
-      const viewedCategories = interactions
-        .filter(interaction => interaction.action === 'view')
-        .map(interaction => interaction.category);
+//       const viewedCategories = interactions
+//         .filter(interaction => interaction.action === 'view')
+//         .map(interaction => interaction.category);
 
-      const searchedCategories = interactions
-        .filter(interaction => interaction.action === 'search')
-        .map(interaction => interaction.category);
+//       const searchedCategories = interactions
+//         .filter(interaction => interaction.action === 'search')
+//         .map(interaction => interaction.category);
 
-      const addedToCartCategories = interactions
-        .filter(interaction => interaction.action === 'add_to_cart')
-        .map(interaction => interaction.category);
+//       const addedToCartCategories = interactions
+//         .filter(interaction => interaction.action === 'add_to_cart')
+//         .map(interaction => interaction.category);
 
-      const allCategories = [...viewedCategories, ...searchedCategories, ...addedToCartCategories];
-      const uniqueCategories = [...new Set(allCategories)];
+//       const allCategories = [...viewedCategories, ...searchedCategories, ...addedToCartCategories];
+//       const uniqueCategories = [...new Set(allCategories)];
 
-      recommendedProducts = await Product.find({ categories: { $in: uniqueCategories } });
-    }
+//       recommendedProducts = await Product.find({ categories: { $in: uniqueCategories } });
+//     }
 
-    res.render('home/home', { products, recommendedProducts });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server Error');
-  }
-};
+//     res.render('home/home', { products, recommendedProducts });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Server Error');
+//   }
+// };
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController');
+const Product = require('../models/product');
+const UserInteraction = require('../models/userInteraction');
 
 // Route to handle rendering the home page and product recommendations
-router.get('/home', homeController.renderHomePage);
+router.get('/home', async (req, res) => {
+  try {
+    const products = await Product.find();
+    let recommendedProducts = [];
+
+    if (req.isAuthenticated()) {
+      const userId = req.user._id;
+      const interactions = await UserInteraction.find({ user: userId });
+
+      const viewedCategories = interactions
+        .filter(interaction => interaction.action === 'view')
+        .map(interaction => interaction.category);
+
+      const searchedCategories = interactions
+        .filter(interaction => interaction.action === 'search')
+        .map(interaction => interaction.category);
+
+      const addedToCartCategories = interactions
+        .filter(interaction => interaction.action === 'add_to_cart')
+        .map(interaction => interaction.category);
+
+      const allCategories = [...viewedCategories, ...searchedCategories, ...addedToCartCategories];
+      const uniqueCategories = [...new Set(allCategories)];
+
+      recommendedProducts = await Product.find({ categories: { $in: uniqueCategories } });
+    }
+
+    res.render('home/home', { products, recommendedProducts });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
