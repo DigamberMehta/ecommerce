2ecb3c2d386b8e47917f69217563bea8e7960cfe
Digamber Mehta <digambermehta2603@gmail.com>
2024-07-25 21:56:38 +0530

    fixes

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { isLoggedIn } = require('../middleware');
 const CartItem = require('../models/cartItem');
-
 const Product = require('../models/product');
 const User = require('../models/user');
 const Wishlist = require('../models/wishlist');
